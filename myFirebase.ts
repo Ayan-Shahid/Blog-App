ea2bd4c8c6fef6e7efd67e3dae60c1dc1d8c8ed9
Ayan-Shahid <63579232+Ayan-Shahid@users.noreply.github.com>
2022-03-19 15:29:23 +0500

    last

--- myFirebase.ts
+++ myFirebase.ts
@@ -1,7 +1,7 @@
 import { getFirestore } from "firebase/firestore";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getStorage, ref } from "firebase/storage";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +18,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 export const fireStore = getFirestore();
 export const storage = getStorage();
